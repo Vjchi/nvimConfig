87f60d5d7871a03b80b789a5db8cd1aa68c4a7af
flescroart <frlescroart@gmail.com>
2023-07-11 18:55:20 +0100

    progressed the bépo layout conversion

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,13 +3,9 @@
 -- Add any additional keymaps here
 
 -- Modify the motions to adapt to the BÉPO layout
--- Moving Down
 vim.keymap.set("n", "t", "j", { silent = true })
--- Moving Up
 vim.keymap.set("n", "s", "k", { silent = true })
--- Moving Right
 vim.keymap.set("n", "r", "l", { silent = true })
--- Moving Left
 vim.keymap.set("n", "c", "h", { silent = true })
 --
 -- Remap t (unTil) to j--
@@ -22,6 +18,24 @@ vim.keymap.set("n", "l", "r", { silent = true })
 vim.keymap.set("n", "k", "", {})
 -- Remap cc (delete [count lines] and insert) to kk
 vim.keymap.set("n", "kk", "cc", {})
+
+-- Modify the <C-x> as well
+vim.keymap.set("n", "<C-c>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<C-t>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<C-s>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<C-r>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+-- Modify the <S-x> as well
+vim.keymap.set("n", "<S-r>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<S-c>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+
+-- Move Lines
+vim.keymap.set("n", "<A-t>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "<A-s>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("i", "<A-t>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<A-s>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<A-t>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "<A-s>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- Leave insert mode by tapping qq
 vim.keymap.set("i", "qq", "<ESC>", { silent = true })
