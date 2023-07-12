f2c6852a3e82be68f58b3d38f71282ed8127ff8e
flescroart <frlescroart@gmail.com>
2023-07-12 07:31:36 +0100

    added configuration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,33 +1,26 @@
--- Keymaps are automatically loaded on the VeryLazy event
+-- Keymaps are automatically joaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+--- This file is automatically loaded by lazyvim.config.init
 
--- Modify the motions to adapt to the BÉPO layout
-vim.keymap.set("n", "t", "j", { silent = true })
-vim.keymap.set("n", "s", "k", { silent = true })
-vim.keymap.set("n", "r", "l", { silent = true })
-vim.keymap.set("n", "c", "h", { silent = true })
---
--- Remap t (unTil) to j--
-vim.keymap.set("n", "j", "t", { silent = true })
--- Remap s (Substitute) to h
-vim.keymap.set("n", "h", "s", { silent = true })
--- Remap r (Replace) to l
-vim.keymap.set("n", "l", "r", { silent = true })
--- Sacrifice c (delete and insert [motion]), and remap k to none
-vim.keymap.set("n", "k", "", {})
--- Remap cc (delete [count lines] and insert) to kk
-vim.keymap.set("n", "kk", "cc", { silent = true })
+local Util = require("lazyvim.util")
 
--- Modify the <C-x> as well
+-- Remap direction keys
+vim.keymap.set({ "n", "x" }, "s", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "t", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "c", "<Left>", { silent = true })
+vim.keymap.set("n", "r", "<Right>", { silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
 vim.keymap.set("n", "<C-c>", "<C-w>h", { desc = "Go to left window", remap = true })
 vim.keymap.set("n", "<C-t>", "<C-w>j", { desc = "Go to lower window", remap = true })
 vim.keymap.set("n", "<C-s>", "<C-w>k", { desc = "Go to upper window", remap = true })
 vim.keymap.set("n", "<C-r>", "<C-w>l", { desc = "Go to right window", remap = true })
 
--- Modify the <S-x> as well
-vim.keymap.set("n", "<S-c>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-vim.keymap.set("n", "<S-r>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Move Lines
 vim.keymap.set("n", "<A-t>", "<cmd>m .+1<cr>==", { desc = "Move down" })
@@ -37,7 +30,15 @@ vim.keymap.set("i", "<A-s>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 vim.keymap.set("v", "<A-t>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 vim.keymap.set("v", "<A-s>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
--- Leave insert mode by tapping àà
-vim.keymap.set("i", "àà", "<Esc>", { silent = true })
--- Save with <C-q>
-vim.keymap.set({ "n", "i" }, "<C-q>", "<cmd>w<cr><Esc>", { silent = true })
+-- buffers
+if Util.has("bufferline.nvim") then
+  vim.keymap.set("n", "<S-c>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+  vim.keymap.set("n", "<S-r>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+  vim.keymap.set("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+  vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+else
+  vim.keymap.set("n", "<S-c>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+  vim.keymap.set("n", "<S-r>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+  vim.keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+  vim.keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+end
